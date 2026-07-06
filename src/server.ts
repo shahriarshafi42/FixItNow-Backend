1ba778b7849f829f5f577fdd6218b3fbbc43b082
Shahriar Ahmed <shafishahriar98@gmail.com>
2026-07-06 23:46:04 +0600

    express setup done

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import app from "./app"
+import config from "./config";
+import { prisma } from "./lib/prisma"
+import "dotenv/config";
+
+const PORT = config.port
+async function main(){
+
+
+    try {
+        // await prisma.$connect()
+        console.log(" connected to database successfully")
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+        
+    } catch (error) {
+        console.error("Error starting server:", error)
+        // await prisma.$disconnect()
+        process.exit(1)
+    }
+}
+
+main()
